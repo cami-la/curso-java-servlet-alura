4e1a22a46048ad19e663666b009674377056acc7
cami <camiladsantoscavalcante@gmail.com>
2021-01-24 06:40:59 -0300

    util class 06.05 - Redirecionamento pelo navegador

--- src/site/camila/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/site/camila/gerenciador/servlet/NovaEmpresaServlet.java
@@ -39,10 +39,12 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		//chamar o JSP
-		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
-		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request, response);
+		response.sendRedirect("listaEmpresas");
+		
+//		//chamar o JSP
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 
 	}
 
