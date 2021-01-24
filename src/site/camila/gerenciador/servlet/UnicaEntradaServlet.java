3f97b7f57233cc5ce5ac827ac2943149745004d9
cami <camiladsantoscavalcante@gmail.com>
2021-01-24 18:49:34 -0300

    util class 01.07 - Implementando o controlador

---
+++ src/site/camila/gerenciador/servlet/UnicaEntradaServlet.java
@@ -0,0 +1,34 @@
+package site.camila.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import site.camila.gerenciador.acao.ListaEmpresas;
+
+@WebServlet("/entrada")
+public class UnicaEntradaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String paramAcao = request.getParameter("acao");
+
+		if (paramAcao.equals("ListaEmpresas")) {
+			
+			ListaEmpresas acao = new ListaEmpresas();
+			acao.executa(request, response);
+			
+		} else if (paramAcao.equals("RemovaEmpresa")) {
+			System.out.println("removendo empresa");
+		} else if (paramAcao.equals("MostraEmpresa")) {
+			System.out.println("mostrando dados da empresa");
+		}
+	}
+
+}
